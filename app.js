a531973992a72b501faca18c78dd61b2fcfba6b1
KhannaDeepika <96528030+KhannaDeepika@users.noreply.github.com>
2022-01-28 13:30:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-res.send(`
-<h1>NodeJS app!</h1>
-`);
-});
-app.get('/error', (req, res) => {
-process.exit(1);
+res.send("NodeJS app");
 });
 app.listen(8080);
